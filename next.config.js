01a3754e0ac73c3865cd44928e50bfb69c3f0105
wgzhaocv <wgzhaocv@gmail.com>
2023-04-26 19:54:31 +0900

    change my user interface

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SERVER_URL: 'http://localhost:3100',
+    HLS_PATH_NEWS:'/hls/news'
+  }
 }
 
 module.exports = nextConfig
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // 配置代理
+  server.use(
+    '/hls',
+    createProxyMiddleware({
+      target: 'http://localhost:3100',
+      changeOrigin: true,
+    })
+  );
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  const port = process.env.PORT || 3000;
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
